078226da5fc7b17973742a9c35545509658ffd29
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-09-20 15:34:22 +0200

    first commit

---
+++ js/import/modules.js
@@ -0,0 +1,8 @@
+import "%modules%/footer/footer";
+import "%modules%/header/header";
+import "%modules%/header_copy/header_copy";
+import "%modules%/header_copy_2/header_copy_2";
+import "%modules%/header_copy_3/header_copy_3";
+import "%modules%/header_copy_4/header_copy_4";
+import "%modules%/header_copy_5/header_copy_5";
+import "%modules%/header_copy_6/header_copy_6";
